5d25fe96c433cf733bb2d70f0498db93adad6828
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-12-05 12:44:52 +0100

    ajout fichier build

---
+++ web-app/build/precache-manifest.f4e419642f916c014ff3bd74f77c4a3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656d24d90592d753f68b62e0154487af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87ad0ea3fcaf0be131cc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "491bf5dc1bed3ac30a03",
+    "url": "/static/js/2.5f1e195d.chunk.js"
+  },
+  {
+    "revision": "87ad0ea3fcaf0be131cc",
+    "url": "/static/js/main.1c221925.chunk.js"
+  },
+  {
+    "revision": "5010d30008aefeaa42fc",
+    "url": "/static/js/runtime-main.974a77f1.js"
+  }
+]);
